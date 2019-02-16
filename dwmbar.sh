404d3156897c513874aa3771a0a1455bc2e04d88
Gavinok <gavinfreeborn@gmail.com>
2019-02-16 13:31:43 -0700

    updated after better remaps.

--- dwmbar.sh
+++ dwmbar.sh
@@ -4,77 +4,81 @@
 
 # This function parses /proc/net/dev file searching for a line containing $interface data.
 # Within that line, the first and ninth numbers after ':' are respectively the received and transmited bytes.
-function get_bytes {
-# Find active network interface
-interface=$(ip route get 8.8.8.8 2>/dev/null| awk '{print $5}')
-line=$(grep $interface /proc/net/dev | cut -d ':' -f 2 | awk '{print "received_bytes="$1, "transmitted_bytes="$9}')
-eval $line
-now=$(date)
-}
+# function get_bytes {
+# # Find active network interface
+# interface=$(ip route get 8.8.8.8 2>/dev/null| awk '{print $5}')
+# line=$(grep $interface /proc/net/dev | cut -d ':' -f 2 | awk '{print "received_bytes="$1, "transmitted_bytes="$9}')
+# eval $line
+# now=$(date)
+# }
+#
+# # Function which calculates the speed using actual and old byte number.
+# # Speed is shown in KByte per second when greater or equal than 1 KByte per second.
+# # This function should be called each second.
+#
+# function get_velocity {
+# value=$1
+# old_value=$2
+# now=$3
+#
+# timediff=$(($now - $old_time))
+# velKB=$(echo "1000000000*($value-$old_value)/1024/$timediff" | bc)
+# if test "$velKB" -gt 1024
+# then
+# 	echo $(echo "scale=2; $velKB/1024" | bc)MB/s
+# else
+# 	echo ${velKB}KB/s
+# fi
+# }
+#
+# # Get initial values
+# get_bytes
+# old_received_bytes=$received_bytes
+# old_transmitted_bytes=$transmitted_bytes
+# old_time=$now
+#
+# # setup for pulseaudio
+# print_volume() {
+# 	volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
+# 	if test "$volume" -gt 0
+# /then
+# 		echo -e "${volume}% 🎵"
+#
+# 	else
+# 		echo -e "Mute 🎵"
+# 	fi
+# }
+#
+# print_wifi() {
+# 	ip=$(ip route get 8.8.8.8 2>/dev/null|grep -Eo 'src [0-9.]+'|grep -Eo '[0-9.]+')
+#
+# 	if=wlan0
+# 		while IFS=$': \t' read -r label value
+# 		do
+# 			case $label in SSID) SSID=$value
+# 				;;
+# 			signal) SIGNAL=$value
+# 				;;
+# 		esac
+# 	done < <(iw "$if" link)
+#
+# 	echo -e "$SSID $SIGNAL $ip WIFI"
+# }
 
-# Function which calculates the speed using actual and old byte number.
-# Speed is shown in KByte per second when greater or equal than 1 KByte per second.
-# This function should be called each second.
-
-function get_velocity {
-value=$1
-old_value=$2
-now=$3
-
-timediff=$(($now - $old_time))
-velKB=$(echo "1000000000*($value-$old_value)/1024/$timediff" | bc)
-if test "$velKB" -gt 1024
-then
-	echo $(echo "scale=2; $velKB/1024" | bc)MB/s
-else
-	echo ${velKB}KB/s
-fi
-}
-
-# Get initial values
-get_bytes
-old_received_bytes=$received_bytes
-old_transmitted_bytes=$transmitted_bytes
-old_time=$now
-
-print_volume() {
-	volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
-	if test "$volume" -gt 0
-	then
-		echo -e "${volume}% VOL"
-	else
-		echo -e "Mute VOL"
-	fi
-}
-
-print_wifi() {
-	ip=$(ip route get 8.8.8.8 2>/dev/null|grep -Eo 'src [0-9.]+'|grep -Eo '[0-9.]+')
-
-	if=wlan0
-		while IFS=$': \t' read -r label value
-		do
-			case $label in SSID) SSID=$value
-				;;
-			signal) SIGNAL=$value
-				;;
-		esac
-	done < <(iw "$if" link)
-
-	echo -e "$SSID $SIGNAL $ip WIFI"
-}
-
-print_mem(){
-	memfree=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
-
-	# memorfree -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }'
-	echo -e "$memfree MEM"
-}
-
+# print_mem(){
+# 	# memfree=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
+#
+# 	mem=$(free | grep Mem | awk '{print ($3/$2 * 100.0) }')
+# 	mem
+# 	# memorfree -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }'
+# 	echo -e "$mem MEM"
+# }
+#
 print_temp(){
 	test -f /sys/class/thermal/thermal_zone0/temp || return 0
 	echo $(head -c 2 /sys/class/thermal/thermal_zone0/temp)C
 }
-
+#
 print_bat(){
 	hash acpi || return 0
 	onl="$(grep "on-line" <(acpi -V))"
@@ -83,11 +87,11 @@ print_bat(){
 	then
 		# suspend when we close the lid
 		#systemctl --user stop inhibit-lid-sleep-on-battery.service
-		echo -e "${charge}% BAT"
+		echo -e "${charge}%" 
 	else
 		# On mains! no need to suspend
 		#systemctl --user start inhibit-lid-sleep-on-battery.service
-		echo -e " ${charge}% BAT"
+		echo -e " ${charge}% "
 	fi
 }
 
@@ -95,14 +99,14 @@ print_date(){
 	date '+%a, %b %d, %Y at %I:%M %p'
 
 }
-
-show_record(){
-	test -f /tmp/r2d2 || return
-	rp=$(cat /tmp/r2d2 | awk '{print $2}')
-	size=$(du -h $rp | awk '{print $1}')
-	echo " $size $(basename $rp)"
-}
-
+#
+# show_record(){
+# 	test -f /tmp/r2d2 || return
+# 	rp=$(cat /tmp/r2d2 | awk '{print $2}')
+# 	size=$(du -h $rp | awk '{print $1}')
+# 	echo " $size $(basename $rp)"
+# }
+#
 while true
 do
 
@@ -114,7 +118,11 @@ do
 	# vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
 
 	# xsetroot -name "$(print_mem) $(print_temp)  $(print_bat) $(show_record) $(print_date)"
-	xsetroot -name "[$(print_volume)] [$(print_temp)] [$(print_mem)] [$(print_bat)] [$(print_date)]"
+	# xsetroot -name "[$(print_date)] [$(print_bat)]"
+	# xsetroot -name "| $(print_volume)% | $(print_temp) ⚙️ | $(print_mem) 📊 | [$(print_date)] [$(print_bat)] "
+	# xsetroot -name "| $(print_temp) ⚙️ | $(print_mem) 📊 | $(print_date) | [$(print_bat)] "
+	# xsetroot -name "|  $(print_temp) ⚙️ |  $(print_date)  | [$(print_bat)] "
+	xsetroot -name "   $(print_date)  |  [$(print_bat)] "
 
 
 	# Update old values to perform new calculations
@@ -122,6 +130,6 @@ do
 	# old_transmitted_bytes=$transmitted_bytes
 	# old_time=$now
 
-	sleep 1
+	sleep 10
 
 done
--- savebat.sh
+++ savebat.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# [ "$(cat /sys/class/power_supply/BAT0/status)" = "Charging" ] && notify-send "here" && exit
 	
  /usr/bin/systemctl hibernate
 
