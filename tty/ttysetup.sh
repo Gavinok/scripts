bd4d7fe48febd9f05e4ce894832b02afc18e3800
Gavinok <gavinfreeborn@gmail.com>
2019-03-31 13:55:27 -0600

    added support for nnn scripts and a tty mode

---
+++ tty/ttysetup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+if [ "$TERM" = "linux" ]; then
+    echo -en "\e]P0232323" #black
+    echo -en "\e]P82B2B2B" #darkgrey
+    echo -en "\e]P1D75F5F" #darkred
+    echo -en "\e]P9E33636" #red
+    echo -en "\e]P287AF5F" #darkgreen
+    echo -en "\e]PA98E34D" #green
+    echo -en "\e]P3D7AF87" #brown
+    echo -en "\e]PBFFD75F" #yellow
+    echo -en "\e]P48787AF" #darkblue
+    echo -en "\e]PC7373C9" #blue
+    echo -en "\e]P5BD53A5" #darkmagenta
+    echo -en "\e]PDD633B2" #magenta
+    echo -en "\e]P65FAFAF" #darkcyan
+    echo -en "\e]PE44C9C9" #cyan
+    echo -en "\e]P7E5E5E5" #lightgrey
+    echo -en "\e]PFFFFFFF" #white
+    clear #for background artifacting
+	# Switch escape and caps if tty:
+	sudo -n loadkeys ~/.scripts/tty/ttymaps.kmap 2>/dev/null
+	tmux -f ~/.scripts/tty/dwm.tmux/dwm.tmux
+fi
+
