c5e01fd0724c6d9532102403d31decd9315421bb
Gavinok <gavinfreeborn@gmail.com>
2019-04-02 13:25:17 -0600

    imptoved tasky integration for compleating, adding, and clearing tasks

--- dmenu/dmenu_googletasks.sh
+++ dmenu/dmenu_googletasks.sh
@@ -4,44 +4,29 @@ EXECUTABLE='python2.7 /home/gavinok/tasky.py'
 PROMPT='to do:'
 createtodo(){
 	newtodo=$(echo "?" | dmenu -p "Enter the new TODO item:")
-	[ -z $newtodo ] &&  exit
-	date=$(echo "?" | dmenu -p "enter date MM/DD/YYYY:")
+	[ -z $newtodo ] || [ $newtodo = "?" ] &&  exit
+	tasklist=$(echo "?" | dmenu -p "Select Tasklist")
+	[ -z $tasklist ] || [ $tasklist = "?" ] &&  tasklist=0
+	date=$(echo "?" | dmenu -p "Enter date MM/DD/YYYY:")
 	if [ -z $date ] || [ $date = "?" ]; then
-		$EXECUTABLE -a --title "$newtodo"
+		$EXECUTABLE -a --tasklist $tasklist --title "$newtodo"
 	else
-		$EXECUTABLE -a -t "$newtodo" -d "$date"
+		$EXECUTABLE -a --tasklist $tasklist --title "$newtodo" -d "$date"
 	fi;
-	#use edit to change anything else
 	exit
 }
-# indexparser(){
-#  sed 's@^[^0-9]*\([0-9]\+\).*@\1@'
-# }
-# toggle(){
-# }
-# remove(){
-# }
-# edit(){
-# }
+toggle(){
+	completed=$(echo $1 | grep -o '[0-9]\+' )
+	$EXECUTABLE -t -i $completed | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g'
+	exit
+}
 mkdir "/tmp/dmenu_todo/"
 touch "/tmp/dmenu_todo/tmp.txt"
 echo "New Todo" > /tmp/dmenu_todo/tmp.txt
 $EXECUTABLE -l | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' | tail -n +2 >> /tmp/dmenu_todo/tmp.txt
-# python2.7 ~/tasky.py -l | sed 's/[\x01-\x1F\x7F]//g' >> /tmp/dmenu_todo/tmp.txt
+echo "clear" >> /tmp/dmenu_todo/tmp.txt
 completed=$(cat /tmp/dmenu_todo/tmp.txt | dmenu -l 10 -p "$PROMPT" |  sed 's/[0-9]. //g')
 [ -z "$completed" ] &&  exit
-if [ "$completed" = "New Todo" ]; then
-	createtodo
-else 
-	res=$(printf "toggle\\nremove\\nedit" | dmenu -i -p 'Power Menu:'  ) 
-	case "$res" in
-		toggle)
-			toggle $completed;;
-		remove)
-			remove $completed;;
-		edit)
-			edit $completed;;
-	esac
-fi;
-# sed -i '/'"$completed"'/d' ~/.calcurse/todo
-
+[ "$completed" = "New Todo" ] && createtodo
+[ "$completed" = "clear" ] &&  $EXECUTABLE -c
+toggle $completed
