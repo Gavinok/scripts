57469a26ef8edeea676af0c770ec24d789f8322a
Gavinok <gavinfreeborn@gmail.com>
2019-04-08 13:18:27 -0600

    better creation of tasks

--- dmenu/dmenu_googletasks.sh
+++ dmenu/dmenu_googletasks.sh
@@ -2,19 +2,23 @@
 # a simple todo script that integrates with calcurse
 EXECUTABLE='python2.7 /home/gavinok/tasky.py'
 PROMPT='to do:'
+# function: createtodo 
+# description: used to create a new task
 createtodo(){
 	newtodo=$(echo "?" | dmenu -p "Enter the new TODO item:")
 	[ -z $newtodo ] || [ $newtodo = "?" ] &&  exit
-	tasklist=$(echo "?" | dmenu -p "Select Tasklist")
-	[ -z $tasklist ] || [ $tasklist = "?" ] &&  tasklist=0
+	tasklist=$($EXECUTABLE -l -s | dmenu -l 3 -p "Select Tasklist" | grep -o '^[0-9]\+')
+	[ -z $tasklist ] &&  tasklist=0
 	date=$(echo "?" | dmenu -p "Enter date MM/DD/YYYY:")
 	if [ -z $date ] || [ $date = "?" ]; then
-		$EXECUTABLE -a --tasklist $tasklist --title "$newtodo"
+		$EXECUTABLE -a  --title "$newtodo" --tasklist $tasklist
 	else
-		$EXECUTABLE -a --tasklist $tasklist --title "$newtodo" -d "$date"
+		$EXECUTABLE -a  --title "$newtodo" -d "$date" --tasklist $tasklist
 	fi;
 	exit
 }
+# function: toggle 
+# description: used to toggle a task between completed and incomplete
 toggle(){
 	completed=$(echo $1 | grep -o '[0-9]\+' )
 	$EXECUTABLE -t -i $completed | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g'
@@ -27,6 +31,9 @@ $EXECUTABLE -l | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' | tail -n +2 >> /tmp/dmenu_tod
 echo "clear" >> /tmp/dmenu_todo/tmp.txt
 completed=$(cat /tmp/dmenu_todo/tmp.txt | dmenu -l 10 -p "$PROMPT" |  sed 's/[0-9]. //g')
 [ -z "$completed" ] &&  exit
+#create a new task
 [ "$completed" = "New Todo" ] && createtodo
+#clear all completed tasks
 [ "$completed" = "clear" ] &&  $EXECUTABLE -c
+#toggle a tasks
 toggle $completed
