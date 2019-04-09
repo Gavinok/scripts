99521e388a1a0c0fe7c740f3d309a18919548264
Gavinok <gavinfreeborn@gmail.com>
2019-04-09 11:46:43 -0600

    added better handeling of bluetooth commands

--- dmenu/dmenu_connection_manager.sh
+++ dmenu/dmenu_connection_manager.sh
@@ -5,11 +5,8 @@ choice=$(printf "Bluetooth\\nNetwork" | dmenu -i -p 'Connect:'  )
 if [ $choice = "Bluetooth" ]; then
 	[ -z $(pgrep blueman) ] && blueman-applet&
 	choice2=$(printf "Connect\\nManage" | dmenu -i -p 'Bluetooth:'  ) 
-	if [ choice2 == "Connect" ];then
-		blueman-assistant
-	else
-		blueman-manager
-	fi
+	[ $choice2 = 'Connect' ] && blueman-assistant
+        [ $choice2 = 'Manage' ] && blueman-manager
 fi
 if [ $choice = "Network" ]; then
 	networkmanager_dmenu
