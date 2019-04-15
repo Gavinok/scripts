b2758e87d5c8248d58c2b87fbdb079a2546a1895
Gavinok <gavinfreeborn@gmail.com>
2019-04-15 12:41:38 -0600

    Now able to interface with Vim for quick help

--- cheat.sh
+++ cheat.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env sh
-curl cheat.sh/$@
+file=$(readlink -f "$1")
+dir=$(dirname "$file")
+# base="${file%.*}"
+# shebang=$(sed -n 1p "$file")
+[ -n "$2" ] && echo xxx; 
+keyword=$2
+cd "$dir" || exit
+
+# shebangtest() {
+# 	case "$shebang" in
+# 		\#\!bash*) curl cheat.sh/bash/$2;;
+# 		*) sent "$file" 2>/dev/null & ;;
+# 	esac
+# }
+
+case "$file" in
+	*\.tex) curl cheat.sh/latex/"$keyword";;
+        *\.c) curl cheat.sh/c/"$keyword";;
+	*\.h)  curl cheat.sh/c/"$keyword";;
+	*\.py)  curl cheat.sh/python/"$keyword";;
+	*\.go) curl cheat.sh/go/"$keyword";;
+	*) curl cheat.sh/"$*";;
+esac
