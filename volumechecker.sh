edb33e1d243a75b64d79455eea2c091e4e1dfb36
Gavinok <gavinfreeborn@gmail.com>
2019-04-21 15:14:59 -0600

    Now uses pactl for better efficiency also now checks for mute

--- volumechecker.sh
+++ volumechecker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # utilises dunstify ant amixer to notify the 
 # user of the volume as well as chang it
-volume="$(amixer -D pulse get Master | awk '$2== "Left:" {print $5}')"
-	/usr/bin/dunstify -r 798 -t 500 "🔊  ${volume}"
+# volume="$(amixer -D pulse get Master | awk '$2== "Left:" {print $5}')"
+volume="$(pactl list sinks | grep 'Mute: yes')"
+[ -z "$volume" ] &&  /usr/bin/dunstify -r 798 -t 500 "🔈 MUTE" && exit
+volume="$(pactl list sinks | grep -m 1 'Volume:' | cut -d/ -f2)"
+	/usr/bin/dunstify -r 798 -t 500 "🔊 ${volume}"
