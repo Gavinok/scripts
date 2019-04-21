8f865a734761e97648f3320a8be1d1b6e2be3c81
Gavinok <gavinfreeborn@gmail.com>
2019-04-21 15:29:22 -0600

    Reversed mute test

--- volumechecker.sh
+++ volumechecker.sh
@@ -3,6 +3,6 @@
 # user of the volume as well as chang it
 # volume="$(amixer -D pulse get Master | awk '$2== "Left:" {print $5}')"
 volume="$(pactl list sinks | grep 'Mute: yes')"
-[ -z "$volume" ] &&  /usr/bin/dunstify -r 798 -t 500 "🔈 MUTE" && exit
+[ -n "$volume" ] &&  /usr/bin/dunstify -r 798 -t 500 "🔈 MUTE" && exit
 volume="$(pactl list sinks | grep -m 1 'Volume:' | cut -d/ -f2)"
 	/usr/bin/dunstify -r 798 -t 500 "🔊 ${volume}"
