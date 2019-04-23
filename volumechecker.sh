7967fbe9cea56c72b4c60e5ab22bdb5827a1a249
Gavinok <gavinfreeborn@gmail.com>
2019-04-22 19:42:09 -0600

    Now works better with headphones and speakers

--- volumechecker.sh
+++ volumechecker.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 # utilises dunstify ant amixer to notify the 
 # user of the volume as well as chang it
-# volume="$(amixer -D pulse get Master | awk '$2== "Left:" {print $5}')"
-volume="$(pactl list sinks | grep 'Mute: yes')"
-[ -n "$volume" ] &&  /usr/bin/dunstify -r 798 -t 500 "🔈 MUTE" && exit
-volume="$(pactl list sinks | grep -m 1 'Volume:' | cut -d/ -f2)"
-	/usr/bin/dunstify -r 798 -t 500 "🔊 ${volume}"
+# Get the index of the selected sink:
+getsink() {
+    pacmd list-sinks | awk '/index:/{i++} /* index:/{print i; exit}'
+}
+# Get the selected sink volume
+getvolume() {
+    volume="$(pactl list sinks | grep 'Mute: yes')"
+    [ -n "$volume" ] &&  /usr/bin/dunstify -r 798 -t 500 "🔈 MUTE" && exit
+
+    volume=$(pacmd list-sinks | awk '/^\svolume:/{i++} i=='"$(getsink)"'{print $5; exit}')
+    /usr/bin/dunstify -r 798 -t 500 "🔊 ${volume}"
+}
+getvolume
