7c4971f03ced9cca0bbc0c0b44daeb611af32e55
Gavinok <gavinfreeborn@gmail.com>
2019-04-25 14:44:26 -0600

    Removed nnn scripts since they are now stored in .config

--- fzedit.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-du -a $1* | awk '{print $2}' | fzf | xargs  -r $EDITOR ;
--- scriptedit.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-du -a ~/.scripts/* ~/.config/* | awk '{print $2}' | fzf | xargs  -r $EDITOR ;
-
