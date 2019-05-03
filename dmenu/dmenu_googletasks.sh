a9bb9716116a783f7fd1acfc0d0e506c21761b2b
Gavinok <gavinfreeborn@gmail.com>
2019-05-03 17:36:36 -0600

    Reduced redundancy in cabl & optimized dmenu_googletasks

--- dmenu/dmenu_googletasks.sh
+++ dmenu/dmenu_googletasks.sh
@@ -6,11 +6,11 @@ PROMPT='to do:'
 # description: used to create a new task
 createtodo(){
 	newtodo=$(echo "?" | dmenu -p "Enter the new TODO item:")
-	[ -z $newtodo ] || [ $newtodo = "?" ] &&  exit
-	tasklist=$($EXECUTABLE -l -s | dmenu -l 3 -p "Select Tasklist" | grep -o '^[0-9]\+')
-	[ -z $tasklist ] &&  tasklist=0
+	[ -z "$newtodo" ] || [ "$newtodo" = "?" ] &&  exit
+	tasklist=$( grep "^[0-9]" "$1"  | dmenu -l 3 -p "Select Tasklist" | grep -o '^[0-9]\+')
+	[ -z "$tasklist" ] &&  tasklist=0
 	date=$(echo "?" | dmenu -p "Enter date MM/DD/YYYY:")
-	if [ -z $date ] || [ $date = "?" ]; then
+	if [ -z "$date" ] || [ "$date" = "?" ]; then
 		$EXECUTABLE -a  --title "$newtodo" --tasklist $tasklist
 	else
 		$EXECUTABLE -a  --title "$newtodo" -d "$date" --tasklist $tasklist
@@ -20,8 +20,8 @@ createtodo(){
 # function: toggle 
 # description: used to toggle a task between completed and incomplete
 toggle(){
-	completed=$(echo $1 | grep -o '[0-9]\+' )
-	$EXECUTABLE -t -i $completed | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g'
+	completed=$(echo "$1" | grep -o '[0-9]\+' )
+	$EXECUTABLE -t -i "$completed" | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g'
 	exit
 }
 mkdir "/tmp/dmenu_todo/"
@@ -30,11 +30,11 @@ echo "New Todo" > /tmp/dmenu_todo/tmp.txt
 $EXECUTABLE -l | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' | tail -n +2 >> /tmp/dmenu_todo/tmp.txt
 linenumber=$(wc -l < /tmp/dmenu_todo/tmp.txt)
 echo "clear" >> /tmp/dmenu_todo/tmp.txt
-completed=$(cat /tmp/dmenu_todo/tmp.txt | dmenu -l $linenumber -p "$PROMPT" |  sed 's/[0-9]. //g')
+completed=$(dmenu -l "$linenumber" -p "$PROMPT" < /tmp/dmenu_todo/tmp.txt |  sed 's/[0-9]. //g' )
 [ -z "$completed" ] &&  exit
 #create a new task
-[ "$completed" = "New Todo" ] && createtodo
+[ "$completed" = "New Todo" ] && createtodo "/tmp/dmenu_todo/tmp.txt"
 #clear all completed tasks
 [ "$completed" = "clear" ] &&  $EXECUTABLE -c
 #toggle a tasks
-toggle $completed
+toggle "$completed"
--- dmenu/spellcheck.sh
+++ dmenu/spellcheck.sh
@@ -5,11 +5,11 @@
 word=$(echo "?" | dmenu -i -p 'spell')
 
 if [ "$word" != "?" ]; then
-	selection=$(echo "$word" | aspell pipe --suggest | sed -e '1d' |sed 's/^[^:]*://g' | sed -e $'s/,/\\\n/g' | dmenu -p 'copy')
-	[ -z "$selection" ] || [ "$selection" == "*" ] && exit
- 	echo $selection | xclip -selection clipboard
- 	echo $selection | xclip -selection primary
-	pgrep -x dunst >/dev/null && notify-send "$(xclip -o -selection clipboard) copied to clipboard."
+    selection=$(echo "$word" | aspell pipe --suggest | sed -e '1d' |sed 's/^[^:]*://g' | sed -e $'s/,/\\\n/g' | dmenu -p 'copy')
+    [ -z "$selection" ] || [ "$selection" == "*" ] && exit
+    echo "$selection" | xclip -selection clipboard
+    echo "$selection" | xclip -selection primary
+    pgrep -x dunst >/dev/null && notify-send "$(xclip -o -selection clipboard) copied to clipboard."
 fi
 exit
 
