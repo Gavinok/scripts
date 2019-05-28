7891cd95340681f0b5526637d9471a0bedbe5018
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2019-05-28 09:58:14 -0400

    commented out the program install since it is not finish

--- updatedotfiles.sh
+++ updatedotfiles.sh
@@ -7,7 +7,7 @@
 # I am making a personal version of this script it is still a work in progress
 ### OPTIONS AND VARIABLES ###
 
-while getopts ":r:h:s" o; do case "${o}" in
+while getopts ":r:h:s:p" o; do case "${o}" in
 	h) printf "Optional arguments for custom use:\\n  -r: Dotfiles repository (local file or url)\\n  -p: Dependencies and programs csv (local file or url)\\n  -a: AUR helper (must have pacman-like syntax)\\n  -h: Show this message\\n" && exit ;;
 	r) dotfilesrepo=${OPTARG} && git ls-remote "$dotfilesrepo" || exit ;;
 	s) scriptrepo=${OPTARG} && git ls-remote "$scriptrepo" || exit ;;
@@ -32,8 +32,8 @@ refreshkeys() { \
 }
 # if -x 
 gitmakeinstall() {
+	echo 'gitmakeinstall'
 	dir=$(mktemp -d)
-	dialog --title "LARBS Installation" --infobox "Installing \`$(basename "$1")\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
 	git clone --depth 1 "$1" "$dir" >/dev/null 2>&1
 	cd "$dir" || exit
 	make >/dev/null 2>&1
@@ -43,7 +43,6 @@ gitmakeinstall() {
 
 manualinstall() { # Installs $1 manually if not installed. Used only for AUR helper here.
 	[ -f "/usr/bin/$1" ] || (
-	dialog --infobox "Installing \"$1\", an AUR helper..." 4 50
 	cd /tmp || exit
 	rm -rf /tmp/"$1"*
 	curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/"$1".tar.gz &&
@@ -52,27 +51,13 @@ manualinstall() { # Installs $1 manually if not installed. Used only for AUR hel
 	sudo -u "$name" makepkg --noconfirm -si >/dev/null 2>&1
 	cd /tmp || return) ;
 }
-# if arch linux or manjaro
-# manualinstall $aurhelper || error "Failed to install AUR helper."
-
-gitmakeinstall() {
-	dir=$(mktemp -d)
-	dialog --title "LARBS Installation" --infobox "Installing \`$(basename "$1")\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
-	git clone --depth 1 "$1" "$dir" >/dev/null 2>&1
-	cd "$dir" || exit
-	make >/dev/null 2>&1
-	make install >/dev/null 2>&1
-	cd /tmp || return ;
-}
 
 aurinstall() { \
-	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
 	echo "$aurinstalled" | grep "^$1$" >/dev/null 2>&1 && return
 	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
 }
 
 pipinstall() { \
-	dialog --title "LARBS Installation" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 5 70
 	command -v pip || pacman -S --noconfirm --needed python-pip >/dev/null 2>&1
 	yes | pip install "$1"
 }
@@ -91,7 +76,6 @@ putgitrepo() { # Downlods a gitrepo $1 and places the files in $2 only overwriti
 
 # if arch or manjaro
 serviceinit() { for service in "$@"; do
-	dialog --infobox "Enabling \"$service\"..." 4 40
 	systemctl enable "$service"
 	systemctl start "$service"
 	done ;
@@ -110,17 +94,18 @@ if [ "$OS" = 'Android' ];then
     pkg install curl git python python-dev zsh wget ranger nnn nvim
 else
     # Check if user is root on Arch distro. Install dialog.
-    pacman -Syu --noconfirm  ||  error "Are you sure you're running this as the root user? Are you sure you're using an Arch-based distro? ;-) Are you sure you have an internet connection?"
+    # pacman -Syu --noconfirm  ||  error "Are you sure you're running this as the root user? Are you sure you're using an Arch-based distro? ;-) Are you sure you have an internet connection?"
+    #
+    # # Refresh Arch keyrings.
+    # refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
+    #
+    # pacman --noconfirm --needed -S base-devel git >/dev/null 2>&1
+    # [ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers # Just in case
+    #
+    # # Allow user to run sudo without password. Since AUR programs must be installed
+    # # in a fakeroot environment, this is required for all builds with AUR.
 
-    # Refresh Arch keyrings.
-    refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
-
-    pacman --noconfirm --needed -S base-devel git >/dev/null 2>&1
-    [ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers # Just in case
-
-    # Allow user to run sudo without password. Since AUR programs must be installed
-    # in a fakeroot environment, this is required for all builds with AUR.
-    newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
+    # newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
 
     # Make pacman and yay colorful and adds eye candy on the progress bar because why not.
     grep "^Color" /etc/pacman.conf >/dev/null || sed -i "s/^#Color/Color/" /etc/pacman.conf
@@ -129,13 +114,14 @@ else
     # Use all cores for compilation.
     sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 
-    manualinstall $aurhelper || error "Failed to install AUR helper."
+    manualinstall "$aurhelper" || error "Failed to install AUR helper."
 
     # The command that does all the installing. Reads the progs.csv file and
     # installs each needed program the way required. Be sure to run this only after
     # the user has been created and has priviledges to run sudo without a password
     # and all build dependencies are installed.
-    installationloop
+
+    # installationloop
 
     # Install the dotfiles in the user's home directory
     putgitrepo "$dotfilesrepo" "$HOME"
