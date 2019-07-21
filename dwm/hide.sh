f43b2416f0ad71d35ab6b40c9ba2c0c0e0b62f1a
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2019-07-21 15:46:51 -0600

    Hide no longer sets the window to float

--- dwm/hide.sh
+++ dwm/hide.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 # printf "%b,%b" "$(xdotool getwindowname "$(xdotool getwindowfocus)")" "$(xdotool windowunmap "$(xdotool getwindowfocus)")" >> $HOME/hidden
 WINDOWID="$(xdotool getwindowfocus)"
-xdotool set_window --classname "float" "$WINDOWID"
+# xdotool set_window --classname "float" "$WINDOWID"
 xdotool windowunmap "$WINDOWID"
 printf "%b@@@%b\\n" "$(xdotool getwindowname "$WINDOWID")" "$WINDOWID" >> "/tmp/hidden"
