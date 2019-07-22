2df2333610ebb51f6aeab04a0cd3fc6952480fdc
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2019-07-21 20:29:00 -0600

    scriptedit now allows for different schemes

--- fzf/scriptedit.sh
+++ fzf/scriptedit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-du -a ~/.scripts/* ~/.config/* | awk '{print $2}' | fzf | xargs  -r $EDITOR ;
+du -a ~/.scripts/* ~/.config/* | awk '{print $2}' | fzf --color="$SCHEME" | xargs  -r $EDITOR ;
 
