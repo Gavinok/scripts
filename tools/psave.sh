a79859a37ea5fe499febbb94bae396a9ff2a0f6d
gavinok <gavinfreeborn@gmail.com>
2019-09-09 09:12:19 -0600

    added surface pro 4 support

--- tools/psave.sh
+++ tools/psave.sh
@@ -23,5 +23,18 @@ cpugov(){
     echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor && echo 1867000 | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq && exit
 }
 
+spcpugov(){
+    full=$(cat /sys/class/power_supply/BAT1/energy_full)
+    now=$(cat /sys/class/power_supply/BAT1/energy_now)
+    state=$(cat /sys/class/power_supply/BAT1/status)
+    [ "$state" = Charging ] && echo 100 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct && exit
+    batt=$(echo "scale=2; 100*($now/$full)" | bc | cut -d"." -f1)
+    [ "$batt" -ge "80" ] && echo 80 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct && exit
+    [ "$batt" -ge "35" ] && echo 50 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct && exit
+    echo 26 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct && exit
+}
+
+device=$(uname -n)
 wifiman
+[ "$device" = "sp4" ] && spcpugov && exit
 cpugov
