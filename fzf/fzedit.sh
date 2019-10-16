f1eb47e95579bedf24e976d308341adaf238fd66
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2019-10-16 13:59:43 -0600

    fzedit now ignores hidden files

--- fzf/fzedit.sh
+++ fzf/fzedit.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-du -a $1* | awk '{print $2}' | fzf | xargs  -r $EDITOR ;
+# du -a $1* | awk '{print $2}' | fzf | xargs  -r $EDITOR ;
+find "$1" -not -path "*/\.*" -type f -print | fzf | xargs  -r $EDITOR ;
