1023155f11f3d88dc5ef41745300426266590c1d
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2019-10-16 14:00:30 -0600

    now ignores .config/coc directory and .git files

--- fzf/scriptedit.sh
+++ fzf/scriptedit.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
-
-du -a ~/.scripts/* ~/.config/* | awk '{print $2}' | fzf --color="$SCHEME" | xargs  -r $EDITOR ;
-
+# du -a ~/.scripts/* ~/.config/* | awk '{print $2}' | fzf --color="$SCHEME" | xargs  -r $EDITOR ;
+find ~/.config/ ~/.scripts/ -path "*/.config/coc" -prune -o -path "*.git" -prune -o -print | fzf | xargs -r $EDITOR
