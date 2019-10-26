88617015e6a95ddd4129686b0e7c54ae87e51288
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2019-10-26 14:54:06 -0600

    now can open files and ignores some directories

--- fzf/scriptedit.sh
+++ fzf/scriptedit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 # du -a ~/.scripts/* ~/.config/* | awk '{print $2}' | fzf --color="$SCHEME" | xargs  -r $EDITOR ;
-find ~/.config/ ~/.scripts/ -path "*/.config/coc" -prune -o -path "*.git" -prune -o -print | fzf | xargs -r $EDITOR
+find ~/.config/ ~/.scripts/ -path "*/.config/coc" -prune -o -path "*.git" -prune -o -print | fzf --bind "ctrl-o:execute-silent(xdg-open {}&)" | xargs -r $EDITOR
