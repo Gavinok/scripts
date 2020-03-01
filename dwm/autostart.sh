2b688c0a6d68f94487d6338730f6ca34443f9f17
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2020-03-01 15:58:31 -0700

    optomised autostart.sh

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -17,9 +17,12 @@ redshift &
 pulsemixer --set-volume 50 &
 xbacklight -set 30 &
 autosuspend.sh  &
-sudo powertop --auto-tune
-dunst
-echo 'auto' | sudo tee '/sys/bus/usb/devices/4-1/power/control'
-psave.sh
+sudo rkill block bluetooth &
+dunst &
+echo 'auto' | sudo tee '/sys/bus/usb/devices/4-1/power/control' &
+# disable Ethernet
+sudo ip link set enp0s25 down &
+sudo powertop --auto-tune &
+psave.sh &
 device=$(uname -n)
 [ "$device" = "sp4" ] && exit
