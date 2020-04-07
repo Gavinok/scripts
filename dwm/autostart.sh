66ccfd024cd405c9648afbc117ed36802e6e9d6b
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2020-04-07 11:56:50 -0600

    removed autostarted programs

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,30 +1,25 @@
 #!/bin/dash
 dock_monitor 
-# included in dock_monitor
-# remaps &
-# setbg &
 killall dwm
 scheme=$(cat ~/.config/colorschemes/current)
-if [ "$scheme" = "dark" ];then
-    compton &
-    flashfocus &
-fi
+# if [ "$scheme" = "dark" ];then
+#     compton &
+#     flashfocus &
+# fi
 unclutter &
 clipmenud &
-# kdeconnect-cli &
-# dropbox-cli start &
-redshift &
+# redshift &
 pulsemixer --set-volume 50 &
 xbacklight -set 30 &
 autosuspend.sh  &
 sudo rkill block bluetooth &
 dunst &
-echo 'auto' | sudo tee '/sys/bus/usb/devices/4-1/power/control' &
+# echo 'auto' | sudo tee '/sys/bus/usb/devices/4-1/power/control' &
 # disable Ethernet
-sudo ip link set enp0s25 down &
+# sudo ip link set enp0s25 down &
 sudo powertop --auto-tune &
 device=$(uname -n)
 [ "$device" = "sp4" ] && exit
 sleep 10
 psave.sh &
-covid19.sh &
+# covid19.sh &
