baa41b26ebbfe3ed75f9a4587ab997741a921a22
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2020-04-13 14:27:39 -0600

    remove dwm debugging

--- dwm/startdwm.sh
+++ dwm/startdwm.sh
@@ -2,7 +2,7 @@
 # Infinitely restart dwm so no programs close
 while true; do
     # Log stderror to a file 
-    dwm 2> /tmp/dwm.log
+    # dwm 2>> /tmp/dwm.log
     # No error logging
-    #dwm >/dev/null 2>&1
+    dwm >/dev/null 2>&1
 done
