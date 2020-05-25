9f6b25bb3563955cd47c9f79941b30a1fadc924f
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2020-05-24 21:10:34 -0600

    setsid is now used in fzf opening using xdg-open

--- fzf/fzedit.sh
+++ fzf/fzedit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # du -a $1* | awk '{print $2}' | fzf | xargs  -r $EDITOR ;
-file=$(find "$1" -not -path "*/\.git*" -type f -print | fzf --bind "ctrl-o:execute-silent(xdg-open {}&)")
-[ -e "$file" ] && ${EDITOR:-vi} $file
+file=$(find "$1" -not -path "*/\.git*" -type f -print | fzf --bind "ctrl-o:execute-silent(setsid xdg-open {}&)")
+[ -e "$file" ] && ${EDITOR:-vi} "$file"
