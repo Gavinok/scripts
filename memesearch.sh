885e2aa6280b78853654dacf99fd00ce83b9f443
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2020-08-06 12:57:06 -0600

    Quick way to search for a png of a meme

---
+++ memesearch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : Gavin Jaeger-Freeborn (gavinfreeborn@gmail.com)
+# @file        : memesearch.sh
+# @created     : Wed 05 Aug 2020 02:14:46 PM
+#
+# @description : Quick why to get a png of a related topic
+######################################################################
+
+xdg-open "https://toppng.com/collection/$*"
+
+# vim: set tw=78 ts=2 et sw=2 sr:
+
