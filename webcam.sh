af4815f0a05c5049c66dd57f8477f6bb5913573a
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2020-08-26 11:20:37 -0600

    Updated webcam.sh

--- webcam.sh
+++ webcam.sh
@@ -9,7 +9,9 @@
 ######################################################################
 
 droidcam-cli adb 4747 &
-mpv av://v4l2:/dev/video0 --profile=low-latency --untimed
+mpv av://v4l2:/dev/video0 --no-osc --profile=low-latency --untimed --geometry=-1920-1080 --autofit=30% --no-resume-playback
+
+# pkill -f /dev/video || mpv --no-osc --no-input-default-bindings --input-conf=/dev/null --geometry=-0-0 --autofit=30%  --title="mpvfloat" /dev/video0
 
 # vim: set tw=78 ts=2 et sw=2 sr:
 
