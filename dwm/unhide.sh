d1cda840cc10d189c4de865d7988ddcb850938b2
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-02-05 19:40:37 -0700

    Better quoting

--- dwm/unhide.sh
+++ dwm/unhide.sh
@@ -6,12 +6,12 @@ HIDDENWINDOWS=$(wc -l $tmpfile | cut -d " " -f1)
 if [ "$HIDDENWINDOWS" -le "1" ];then
     WINDOWID=$(sed 's/@@@/ /g' "$tmpfile" | awk '{print $NF}' )
 else
-    WINDOWID=$(sed 's/@@@/ /g' "$tmpfile" | dmenu -p "Show" -l $HIDDENWINDOWS | awk '{print $NF}' )
+    WINDOWID=$(sed 's/@@@/ /g' "$tmpfile" | dmenu -p "Show" -l "$HIDDENWINDOWS" | awk '{print $NF}' )
 fi
 
 if [[ -n "$WINDOWID" ]]; then
     xdotool windowmap "$WINDOWID"
-    sed -i '/'${WINDOWID}'/d' "$tmpfile"
+    sed -i '/'"${WINDOWID}"'/d' "$tmpfile"
     export WINDOWID=
 else
     notify-send "No hidden windows"
