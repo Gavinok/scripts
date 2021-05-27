ecf21f6d02602293af5b866486153a2cc0714fe4
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2021-05-27 12:59:24 -0600

    removed old scripts

--- fzf/fzedit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# du -a $1* | awk '{print $2}' | fzf | xargs  -r $EDITOR ;
-file=$(find "$1" -not -path "*/\.git*" -type f -print | fzf --bind "ctrl-o:execute-silent(setsid xdg-open {}&)")
-[ -e "$file" ] && ${EDITOR:-vi} "$file"
--- fzf/scriptedit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# du -a ~/.scripts/* ~/.config/* | awk '{print $2}' | fzf --color="$SCHEME" | xargs  -r $EDITOR ;
-file=$(find ~/.config/ ~/.scripts/ -path "*/.config/coc" -prune -o -path "*.git" -prune -o -print | fzf --bind "ctrl-o:execute-silent(setsid xdg-open {} &)")
-[ -e "$file" ] && ${EDITOR:-vi} $file
