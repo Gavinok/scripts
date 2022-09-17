85a2d86790498266e1781a71046aafd4c1d5b72c
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-09-17 10:14:18 -0700

    update

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-dock_monitor 
+dock_monitor
 scheme=$(cat ~/.config/colorschemes/current)
 # if [ "$scheme" = "dark" ];then
-    picom --experimental-backends &
-    flashfocus &
+picom --experimental-backends &
+flashfocus &
 # fi
 # unclutter &
 clipmenud &
 
-#       LATITUDE:LONGITUDE &
-redshift -l 60:-135 &
+#          LATITUDE:LONGITUDE
+redshift -t 3400:3200 -l 60:-135 &
 # pulsemixer --set-volume 50 &
 xbacklight -set 30 &
-autosuspend.sh  &
+autosuspend.sh &
 # sudo rkill block bluetooth &
 dunst &
 # echo 'auto' | sudo tee '/sys/bus/usb/devices/4-1/power/control' &
@@ -24,6 +24,8 @@ if [ -e /usr/lib/kdeconnectd ]; then
     setsid /usr/lib/kdeconnectd &
 fi
 
+# startlemon.sh  | lemonbar -f "monospace" -f "Symbols Nerd Font-10" &
+# xbindkeys &
 remaps 2>/dev/null &
 setsid syncthing --no-browser &
 device=$(uname -n)
@@ -32,3 +34,5 @@ xinput disable "Elan Touchpad"
 # psave.sh &
 # covid19.sh &
 # remind -z -k'notify-send 🦄\ [REMINDER]:\ %s" &' "${DOTREMINDERS}" &
+# xinput set-prop 'Elan TrackPoint' 'Coordinate Transformation Matrix' \
+#        9.0 0.0 0.0 0.0 9.0 0.0 0.0 0.0 1.0
